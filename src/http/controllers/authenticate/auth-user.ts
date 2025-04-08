778c46284432f679add688eb75aa0f8103ff1c03
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-04-08 19:57:46 -0400

    Feat: Creating auth routes

---
+++ src/http/controllers/authenticate/auth-user.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { Request, Response } from "express";
+import { authenticateUserRepository } from "../../../lib/auth-service";
+
+const authUser = async (req: Request, res: Response) => {
+  const schema = z.object({
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
+  });
+
+  const body = req.body;
+  const { email, password } = schema.parse(body);
+
+  const response = await authenticateUserRepository(email, password);
+
+  if (!response) {
+    res.status(400).json({ message: "Usuário ou senha incorretos." });
+  }
+
+  res.status(200).json({
+    message: "Usuário autenticado com sucesso",
+    data: response,
+  });
+};
+
+export default authUser;

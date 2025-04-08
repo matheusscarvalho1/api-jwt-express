c1435bad14e72d1d6f1f3d17bd06172cbbc58408
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-04-08 19:59:53 -0400

    Feat: Registring new routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import { userRoutes } from "./http/controllers/usuarios/routes";
+import { userRoutes } from "./http/controllers/users/routes";
+import { unprotectedRoutes } from "./http/controllers/unprotected/routes";
+import { protectedRoutes } from "./http/controllers/protected/routes";
+import { authRoutes } from "./http/controllers/authenticate/routes";
 
 dotenv.config();
 const app = express();
-
 // garente que todo body seja convertido em .json automaticamente
 app.use(express.json());
+
 userRoutes(app);
+unprotectedRoutes(app);
+protectedRoutes(app);
+authRoutes(app);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);

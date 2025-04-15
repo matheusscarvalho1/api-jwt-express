2dd899966d812c15e418e28ec4957a3106f10408
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-04-14 20:47:46 -0400

    Feat: Adding auth verification on user routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,15 +4,28 @@ import createUser from "./create-user";
 import updateUser from "./update-user";
 import deleteUser from "./detele-user";
 import getUserById from "./get-user-by-id";
+import authMiddleware from "../../../middleware/authMiddleware";
 
 export function userRoutes(app: express.Application) {
   const router = express.Router();
 
   router.post("/v1/user/create", createUser as RequestHandler);
-  router.get("/v1/user/get", getUsers as RequestHandler);
-  router.get("/v1/user/get/id/:id", getUserById as RequestHandler);
-  router.put("/v1/user/update/id/:id", updateUser as RequestHandler);
-  router.delete("/v1/user/delete/id/:id", deleteUser as RequestHandler);
+  router.get("/v1/user/get", authMiddleware, getUsers as RequestHandler);
+  router.get(
+    "/v1/user/get/id/:id",
+    authMiddleware,
+    getUserById as RequestHandler
+  );
+  router.put(
+    "/v1/user/update/id/:id",
+    authMiddleware,
+    updateUser as RequestHandler
+  );
+  router.delete(
+    "/v1/user/delete/id/:id",
+    authMiddleware,
+    deleteUser as RequestHandler
+  );
 
   app.use("/api", router);
 }

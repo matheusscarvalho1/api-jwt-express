080c1c0e01dc92fe226feafa319995fa3b0b5eb3
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-07-15 12:48:27 -0400

    Feat: nova rota para verificar saude

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+})
+
 app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+  console.log("Server is running on http://localhost:8080");
 });
